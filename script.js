c70d006088824d3b1372c44d4fdb81e85d40308c
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-09-23 17:31:40 +0200

    Setup constructor for each book, going to create page design on figma to assist frontend

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('connected')
+let myLibrary = [];
+
+function Book(title, author, pages, pagesRead, complete) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.pagesRead = pagesRead;
+  this.complete = complete;
+}
+
+function addBookToLibrary() {
+}
