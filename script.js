39d3bab1216b48a27e87d9bfa432b95a8b62468a
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-09-28 16:43:28 +0200

    New books are now being created through a javascript constructor before being saved to an array

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 const newBook = document.getElementById('new')
 const newBookTwo = document.getElementById('newBook')
-const form = document.getElementById('form')
+const formHtml = document.getElementById('formAll')
 const noBook = document.getElementById('noBook')
 const cancel = document.getElementById('cancel')
 const formContent = document.getElementById('formContent')
+
 const formSubmit = document.getElementById('submit')
-
 formSubmit.addEventListener('click', () => {
-  let bookData = Array.from(document.querySelectorAll('#createBook input[type="text"]'))
-  let readStatus = document.querySelector('#createBook input[type="checkbox"]')
-  let pairData = bookData.reduce((acc, input) => ({ ...acc, [input.id]: input.value }), {})
-  console.log(pairData, readStatus.checked)
-})
+  addBookToLibrary()
+  document.getElementById('form').reset()
+}) 
+  
 
-const formButtons = [newBook, newBookTwo, cancel]
+const formButtons = [newBook, newBookTwo, cancel, formSubmit]
+for (let i = 0; i < formButtons.length; i++) {
+  formButtons[i].addEventListener('click', () => {
+    hideForm()
+  })
+}
 
 let reading = [1];
 let read = [];
 
-function Book(title, author, imgUrl, complete) {
+function Book(title, author, imgUrl, read) {
   this.title = title;
   this.author = author;
   this.imgUrl = imgUrl;
-  this.complete = complete;
+  this.read = read;
 }
 
 function addBookToLibrary() {
+  let bookData = Array.from(document.querySelectorAll('#form input[type="text"]'))
+  let pairData = bookData.reduce((acc, input) => ({ ...acc, [input.id]: input.value }), {})
+  let readState = document.querySelector('#form input[type="checkbox"]').checked
+  let title = pairData.title
+  let author = pairData.author
+  let imgUrl = pairData.imgUrl
+  const newBook = new Book(title, author, imgUrl, readState)
+  if (readState) {
+    read.push(newBook)
+  } else {
+    reading.push(newBook)
+  }
 }
 
 (() => {
@@ -35,17 +51,12 @@ function addBookToLibrary() {
 })()
 
 function hideForm() {
-  if (form.classList.contains('hiddenForm')) {
-    form.classList.remove('hiddenForm')
+  if (formHtml.classList.contains('hiddenForm')) {
+    formHtml.classList.remove('hiddenForm')
     formContent.classList.remove('hiddenFormContent')
   } else {
-    form.classList.add('hiddenForm')
+    formHtml.classList.add('hiddenForm')
     formContent.classList.add('hiddenFormContent')
   }
 }
 
-for (let i = 0; i < formButtons.length; i++) {
-  formButtons[i].addEventListener('click', () => {
-    hideForm()
-  })
-}
